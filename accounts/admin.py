b14212566b83d8e72463d3100ea7c29103d6cf68
rvngrd <rrvngrdd@gmail.com>
2023-01-15 17:41:34 +0330

    trying to show tables in django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+
+
+@admin.register(Employee)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = ['user.get_full_name()', 'ncode', 'rank']
