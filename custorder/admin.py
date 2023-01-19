c8d6a2e4b72d3258599f0c363339a4375dc4581c
rvngrd <rrvngrdd@gmail.com>
2023-01-19 21:03:31 +0330

    adding inlines and list display to custorder/admin.py

--- custorder/admin.py
+++ custorder/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from django_jalali.admin.filters import JDateFieldListFilter
+from .models import CustomerBuy, Product, BuyItem
 # Register your models here.
+
+
+class ItemInline(admin.TabularInline):
+    model = BuyItem
+
+
+@admin.register(CustomerBuy)
+class Admin(admin.ModelAdmin):
+    inlines = [ItemInline]
+    list_display = ['id', 'employee', 'customer', 'datetime', 'description']
+    list_filter = [
+         ('datetime', JDateFieldListFilter),
+    ]
+    search_fields = (
+        'id',
+    )
