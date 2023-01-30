1d391175b86d4b15359a2fce603bfed2b75c96c3
rvngrd <rrvngrdd@gmail.com>
2023-01-30 16:51:12 +0330

    adding prodorder to installed_apps

--- kargah/settings.py
+++ kargah/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'custorder.apps.CustorderConfig',
+    'prodorder.apps.ProdorderConfig',
     'django_jalali'
 ]
 
