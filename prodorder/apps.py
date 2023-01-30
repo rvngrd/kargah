b217068663b3be79fab9eb59ea3eafeea875d99b
rvngrd <rrvngrdd@gmail.com>
2023-01-30 17:02:52 +0330

    changing verbose name of prodorder app

--- prodorder/apps.py
+++ prodorder/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ProdorderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prodorder'
+    verbose_name = 'دستور تولید'
