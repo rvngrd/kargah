87b64b940381baa62a70e041ce63d76943c36f23
rvngrd <rrvngrdd@gmail.com>
2023-01-30 17:01:28 +0330

    ProductionOrder model created

--- prodorder/models.py
+++ prodorder/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django_jalali.db import models as jmodels
 
 # Create your models here.
+
+
+class ProductionOrder(models.Model):
+    """
+    Represents production order model
+    """
+
+    class Meta:
+        verbose_name = 'ثبت تولید'
+        verbose_name_plural = 'ثبت تولید'
+    objects = jmodels.jManager()
+    employee = models.ForeignKey('accounts.Employee', on_delete=models.PROTECT, verbose_name='کارمند')
+    product = models.ForeignKey('custorder.Product', on_delete=models.PROTECT, verbose_name='محصول')
+    quantity = models.IntegerField('تعداد')
+    datetime = jmodels.jDateTimeField('تاریخ', auto_now_add=True)
+    description = models.TextField('توضیحات', blank=True, null=True)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.employee, self.product, self.quantity)
